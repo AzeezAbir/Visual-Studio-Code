2e1d2c7be2bad29a93d477d1b0af498521c8ffbf
Tîme ķílĺìər <azeezabir28@gmail.com>
2022-11-14 19:23:13 +0530

    Update index.js

--- Temp/index.js
+++ Temp/index.js
@@ -1,1 +1,17 @@
-console.log('Hello!')
+const http = require('http')
+const { connected } = require('process')
+const port = 3000
+
+const server = http.createServer(function(req, res) {
+    res.write('Hello Node')
+    res.end()
+})
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something went wrong!', error)
+    }
+    else {
+        console.log('Server is listening on port' + ' ' + port)
+    }
+})
